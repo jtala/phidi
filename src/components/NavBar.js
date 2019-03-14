c5ac28d208855c9eebb384cf090b1aac08d53c0c
Jonathan Talavera <jtalavera@g.ucla.edu>
2019-03-14 15:10:02 -0700

    fixed headers

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="container">
+      <nav className="navbar fixed-top navbar-light bg-light">
+  <a className="navbar-brand" href="#">Fixed top</a>
+</nav>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+    <p>Sanctus clita amet duo labore sed ipsum. Dolor sit takimata stet sed elitr. Consetetur stet clita ipsum dolore, voluptua ut.</p><br></br>
+   
+    </div>
+  );
+}
+
+export default NavBar;
