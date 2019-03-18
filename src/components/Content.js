5d90e8380a6520fac4fefeab95719e7494790c8f
Jonathan Talavera <jtalavera@g.ucla.edu>
2019-03-18 15:10:46 -0700

    dummy text

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,20 +2,26 @@ import React from "react";
 
 function Content () {
     return (
-        <div>
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
-        Lorem Lorem Lorem
+        <div className ='content'>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
+        <p>Vel provident aliquam inventore dolore quod consequuntur itaque. Enim sit fuga. Reiciendis rem et rerum aut amet iusto qui nostrum. Aliquid aliquid fugiat quis placeat illum dolorum eum eum.</p>
         </div>
     )
 }
