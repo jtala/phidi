4e7f8ad7aed24f5f50e41fd0a519610afae95673
Jonathan Talavera <jtalavera@g.ucla.edu>
2019-04-02 10:27:51 -0700

    added images

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,11 +1,59 @@
 import React , {Component} from "react";
+import './Content.css';
+import image1 from './images/PHIDI_C1.PNG';
+import image2 from './images/PHIDI_C2.PNG';
+import image3 from './images/PHIDI_C3.PNG';
+import image4 from './images/PHIDI_C4.jpg';
+import image5 from './images/PHIDI_C5.jpg';
 
 class Content extends Component {
     render (){
         return (
             <div className ='content'>
-            Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
-            <br></br>
+                <div className="container">
+                    <div className='row'>
+                        <div className="col-md-4">
+                        <img src={image1} width='100%' padding='20px'></img>
+                        <br></br>
+                        Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
+                        </div>
+
+                        <div className="col-md-4">
+                        <img src={image2} width='100%' padding='20px'></img>
+
+                        Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
+                        <br></br>
+                        </div>
+                        <div className="col-md-4">
+                        <img src={image3} width='100%' padding='20px'></img>
+
+                        Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
+                        <br></br>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className='row'>
+                        <div className="col-md-4">
+                        <img src={image4} width='100%' padding='20px'></img>
+                        <br></br>
+                        Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
+                        </div>
+
+                        <div className="col-md-4">
+                        <img src={image5} width='100%' padding='20px'></img>
+
+                        Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
+                        <br></br>
+                        </div>
+                        <div className="col-md-4">
+                        <img src={image3} width='100%' padding='20px'></img>
+
+                        Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
+                        <br></br>
+                        </div>
+                    </div>
+                </div>
+            
 
             Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
             <br></br>
@@ -15,13 +63,6 @@ class Content extends Component {
 
             Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
             <br></br>
-
-            Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
-            Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
-            Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
-            Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
-            Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
-            Sit vero corporis id et laborum repellendus omnis nobis. Est amet tempore. Aliquid laborum vero officiis earum neque.
             </div>
         );
     }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './NavBar.css';
+import Logo from "./images/PhiDiLogo.png";
 
 function NavBar() {
   return (
     <div className="container">
       <nav className="navbar fixed-top navbar-light bg-light">
-        <a className="navbar-brand" href="#">PhiDi</a>
+        <img className="navbar-brand" src={Logo} width='25%'/>
       </nav>
     </div>
   );
